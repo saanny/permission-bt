55dc6f0f439af0b097de8cd0e1225148b319d113
amir ahmadi <amirahmadidev@gmail.com>
2024-12-22 18:35:43 +0330

    fix: add jwt module

--- src/services/service.module.ts
+++ src/services/service.module.ts
@@ -1,4 +1,7 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { AuthConfigModule } from 'src/config/auth/auth-config.module';
+import { AuthConfigService } from 'src/config/auth/auth-config.service';
 import { RedisModule } from 'src/database/redis/redis.module';
 import { RepositoriesModule } from 'src/repositories/repositories.module';
 import { GroupsServiceImpl } from 'src/services/groups.service';
@@ -11,7 +14,23 @@ import { TweetsServiceImpl } from 'src/services/tweets.service';
 import { UsersServiceImpl } from 'src/services/users.service';
 
 @Module({
-  imports: [RepositoriesModule, RedisModule],
+  imports: [
+    RepositoriesModule,
+    RedisModule,
+
+    JwtModule.registerAsync({
+      inject: [AuthConfigService],
+      imports: [AuthConfigModule],
+      useFactory(authConfigService: AuthConfigService) {
+        return {
+          secret: authConfigService.JWT_SECRET,
+          signOptions: {
+            expiresIn: Number(authConfigService.JWT_EXPIRES_IN_SECONDS),
+          },
+        };
+      },
+    }),
+  ],
   providers: [
     {
       provide: GroupsService,
